4e02d058c5053479e8b59daef875c6acd867a228
pavel <mail@east-tech.by>
2019-02-25 10:31:40 +0300

    psr

--- database/seeds/ForumCategoriesTableSeeder.php
+++ database/seeds/ForumCategoriesTableSeeder.php
@@ -3,25 +3,23 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ForumCategoriesTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $categories = [];
+class ForumCategoriesTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$categories = [];
 
-        for ($i = 1; $i <= 11; $i++) {
-            $cName = 'Проект #' . $i;
+		for ( $i = 1; $i <= 11; $i ++ ) {
+			$cName = 'Проект #' . $i;
 
-            $categories[] = [
-                'title' => $cName,
-            ];
-        }
+			$categories[] = [
+				'title' => $cName,
+			];
+		}
 
-        DB::table('forum_categories')->insert($categories);
-    }
+		DB::table( 'forum_categories' )->insert( $categories );
+	}
 }
